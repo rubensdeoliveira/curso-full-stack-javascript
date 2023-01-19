5859b0f02521b13dde4c4aa5aae66db60da68bc1
Luana <ldeoliveiratech@gmail.com>
2023-01-19 16:21:10 +0100

    alterações nomes arquivos

--- aula06/exemplo07-operations.js
+++ aula06/exemplo07-operations.js
@@ -1,6 +1,6 @@
 // Operações básicas
 
-const operacaoSoma = 1 + 1
+const operacaoSoma = 1 + 1  //tem que botar parseInt senao concatena ?
 const operacaoSubtracao = 1 - 1
 const operacaoMultiplicar = 1 * 1
 const operacaoDividir = 1 / 1
